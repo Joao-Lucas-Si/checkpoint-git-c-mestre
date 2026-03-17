388ba8adc3486696ba241ae6572dc7badf822e87
joaopdrojpev-coder <joaopedrojpev@gmail.com>
2026-03-17 11:14:34 -0300

    Adicionei meu nome

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 
 int main() {
- char joaoRibeiroNome="joao pedro ribeiro santos";   
+ char joaoRibeiroNome="joao pedro ribeiro santos";  
+ char joaoPedroNome="Joao Pedro Evangelista de Almeida";
 }
