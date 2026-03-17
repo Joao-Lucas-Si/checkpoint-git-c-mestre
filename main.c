5efd18db5f73226f93c9b02a290242cfa57e00f1
joao-lucas-si <joao6lucas2168@gmail.com>
2026-03-17 11:21:44 -0300

    adicionando array de nomes

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
 #include<stdio.h>
 
 int main() {
- char joaoRibeiroNome="joao pedro ribeiro santos";  
- char joaoPedroNome="Joao Pedro Evangelista de Almeida";
-char EnzoribeiroNome="Enzo Ribeiro";
+    char joaoRibeiroNome="joao pedro ribeiro santos";  
+    char joaoPedroNome="Joao Pedro Evangelista de Almeida";
+    char EnzoribeiroNome="Enzo Ribeiro";
+    char joaoLucasNome = "João Lucas Silva Lopes";
+
+    char nomes[5] = {
+        joaoRibeiroNome,
+        joaoPedroNome,
+        EnzoribeiroNome,
+        joaoLucasNome
+    };
 
 }
